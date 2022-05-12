2801bcae2edf2b4d64d9eb392f0248a931c6afc1
“seeinplays” <seeinplays@gmail.com>
2022-05-12 15:05:59 -0600

    imports clean up

--- src/main/java/com/seeinplays/spruce/models/Message.java
+++ src/main/java/com/seeinplays/spruce/models/Message.java
@@ -1,7 +1,5 @@
 package com.seeinplays.spruce.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
